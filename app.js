3ff573d6de79a6f1067fe9e851e2ed046d540f90
tylarfu <tylarcorey@gmail.com>
2025-03-20 18:14:48 +0000

    css done and js added to home

---
+++ app.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("load", function () {
+    // Select the image element by ID
+    const image = document.getElementById("image");
+
+    // Remove the "hidden" class and add the "visible" class
+    image.classList.remove("hidden");
+    image.classList.add("visible");
+});
